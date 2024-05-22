266c929e876738b27d6571213c4eb330572cb66d
Ivaldi <bran.jacobson@gmail.com>
2024-05-21 23:15:26 -0700

    background 5

---
+++ background.js
@@ -0,0 +1,44 @@
+class Layer {
+    constructor(game, width, height, speedModifier, image){
+        this.game = game;
+        this.width = width;
+        this.height = height;
+        this.speedModifier = speedModifier;
+        this.image = image;
+        this.x = 0;
+        this.y = 0;
+    }
+    update(){
+        if (this.x < -this.width) this.x = 0;
+        else this.x -= this.game.speed = this.speedModifier;
+    }
+    draw(context){
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+export class Background {
+    constructor(game){
+        this.game = game;
+        this.width = 1667;
+        this.height = 500;
+        this.layer5image = document.getElementById('layer5');
+        this.backgroundLayers = [];
+        this.layer1 = new Layer(this.game, this.width, this.height, 1, this.layer5image)
+        this.layer4image = layer4;
+        this.layer3image = layer3;
+        this.layer2image = layer2;
+        this.layer1image = layer1;
+
+    }
+    update(){
+        this.backgroundLayers.forEach(layer => {
+            layer.update();
+        })
+    }
+    draw(context){
+        this.backgroundLayers.forEach(layer => {
+            layer.draw(context);
+        })
+    }
+}
