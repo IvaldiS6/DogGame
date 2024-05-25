3566dc1f528ef7110cab9ae38e8806c54688105b
Ivaldi <bran.jacobson@gmail.com>
2024-05-24 23:52:33 -0700

    running particle effects

--- main.js
+++ main.js
@@ -22,11 +22,14 @@ window.addEventListener('load', function(){
             this.input = new InputHandler(this);
             this.UI = new UI(this);
             this.enemies =[];
+            this.particles = [];
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
             this.debug = true;
             this.score = 0;
             this.fontColor = 'black';
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();
         }
         update(deltaTime){
             this.background.update();
@@ -42,6 +45,11 @@ window.addEventListener('load', function(){
                 enemy.update(deltaTime);
                 if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
             });
+            // handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if (particle.markedForDeletion) this.particles.splice(index, 1);
+            });
         }
         draw(context){
             this.background.draw(context);
@@ -49,18 +57,19 @@ window.addEventListener('load', function(){
             this.enemies.forEach(enemy => {
                 enemy.draw(context);
             });
+            this.particles.forEach(particle => {
+                particle.draw(context);
+            });
             this.UI.draw(context);
         }
         addEnemy(){
             if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
             else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
             this.enemies.push(new FlyingEnemy(this));
-            console.log(this.enemies);
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
-    console.log(game);
     let lastTime = 0;
 
     function animate(timeStamp){
